3e8569d738652a6af1a16720bb20e6cf7ca836a1
Kenny Darrell <darrelkj@gmail.com>
2014-04-06 22:26:59 -0400

    add read.manifest

--- R/manifest.R
+++ R/manifest.R
@@ -4,13 +4,13 @@ x <- 1
 y <- 2
 z <- 3
 
-data <- cars
+myData <- cars
 
-data$date <- Sys.Date() + sample(1:10, nrow(data), replace = TRUE)
+myData$date <- Sys.Date() + sample(1:10, nrow(myData), replace = TRUE)
 
-data2 <- data[data$date > Sys.Date()+1, ]
+myData2 <- myData[myData$date > Sys.Date()+1, ]
 
-data2$rand <- sample(c(x, y, z), nrow(data2), replace = TRUE)
+myData2$rand <- sample(c(x, y, z), nrow(myData2), replace = TRUE)
 
 
 
@@ -38,6 +38,7 @@ manifest <- function(..., hist = F, file = stop("'file' must be specified")) {
   evl <- function(x) eval(parse(text = x))
   print('Comments: ')
   notes <- scan(what = character(), quiet = T)
+  if (length(notes) == 0) notes <- 'These are the notes you would have typed to explain the data.'
   #summary <- lapply(loc, function(a) list(a, class(a), eval(parse(text = a))))
   manifest <- list(summary = ldply(loc, function(a) c(name = a, 
                                                       class = class(evl(a)), 
@@ -62,7 +63,19 @@ manifest <- function(..., hist = F, file = stop("'file' must be specified")) {
 
 manifest(file = 'x.RData')
 
-manifest(x, y, file = 'x.RData')
+
+read.manifest <- function(file) {
+  load(file, envir = .GlobalEnv)
+  disp <- paste('The file ' , file, ' was created by ', manifest$createdby, 
+                ' on ', manifest$time, '.', sep = '')
+  message(disp)
+  cat('\n')
+  message(manifest$notes)
+  cat('\n')
+  manifest['summary']
+}
+
+read.manifest('x.RData')
 
 
 
